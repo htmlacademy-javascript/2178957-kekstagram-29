0f96700b8ed71c637e0a72740bae8882b8d9cd13
Вячеслав <vyacheslav.belov.01@mail.ru>
2023-06-12 19:50:02 +0300

    Добавляет наставника

---
+++ js/functions.js
@@ -0,0 +1,6 @@
+function lengthString (string,length) {
+  const result = (string.length <= length) ? console.log(true) : console.log(false);
+  return (string.replaceAll(' ', ''));
+};
+
+lengthString('проверяемая строка', 50);
